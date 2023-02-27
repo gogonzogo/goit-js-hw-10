a9f9ff6b30352e11ec5d023be13a52880f01a644
Jose Gonzales <jose.gonzales08@ymail.com>
2023-02-27 11:18:39 -0500

    update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ ref.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 function handleInput(e) {
   e.preventDefault();
   let inputValue = e.target.value.trim();
-  if (!inputValue) {
+  if (inputValue === '') {
+    ref.countryList.innerHTML = '';
+    ref.countryInfo.innerHTML = '';
     return;
   }
 
@@ -24,37 +26,52 @@ function handleInput(e) {
     .then((data) => {
       console.log(data);
       if (data.length > 10) {
+        ref.countryList.innerHTML = '';
+        ref.countryInfo.innerHTML = '';
         Notify.info("Too many matches found. Please enter a more specific name.");
         return;
-      } else if (data.length > 2 && data.length <= 10) {
-        console.log(data.length > 2 && data.length <= 10);
-        countryListMarkup(data);
-      } else if (data.length === 1) {
-        countryInfoMarkup(data);
       };
+      
+      if (data.length >= 2 && data.length <= 10) {
+        console.log(data.length >= 2 && data.length <= 10);
+        ref.countryInfo.innerHTML = '';
+        const countryListMarkUp = createCountryListMarkup(data);
+        ref.countryList.innerHTML = countryListMarkUp;
+      };
+      
+      if (data.length === 1) {
+        ref.countryList.innerHTML = '';
+        const countryInfoMarkup = createCountryInfoMarkup(data);
+        ref.countryInfo.innerHTML = countryInfoMarkup;
+      };
+
     })
     .catch((error) => {
+      ref.countryList.innerHTML = '';
+      ref.countryInfo.innerHTML = '';
       Notify.failure("Oops, there is no country with that name");
     });
   };
 
-function countryListMarkup(countriesData) {
-  return data.map(country => {
-    `<li>
-      <svg width="70" height="50"><use href="${country.flags.svg}"></use></svg>
-      <p>${country.name.official}</p>
-    </li>`;
-  }).join('');
+function createCountryListMarkup(countriesData) {
+  return countriesData.map((country) =>
+    `<li class="country-list__item">
+      <img class="country-list__img" src="${country.flags.svg}" alt="${country.flags.alt}" width="40" height="20">
+      <p class="country-list__country-name">${country.name.official}</p>
+    </li>`
+  ).join(' ');
 };
 
-function countryInfoMarkup(countryData) {
-  return countryData.map(country => {
-    return `
-      <svg width="100" height="80"><use href="${country.flags.svg}"></use></svg>
-      <h1>${country.name.offical}</h1>
-      <p><b>Capital:</b>${country.capital}</p>
-      <p><b>Population:</b>${country.population}</p>
-      <p><b>Languages:</b>${country.languages}</p>`;
-  }).joing(' ');
+function createCountryInfoMarkup(countryData) {
+  return countryData.map((country) => 
+    `
+    <div class="country-info__img-header-container">
+    <img class="country-info__img" src="${country.flags.svg}" alt="${country.flags.alt}" width="40" height="20">
+    <h1 class="country-info__header">${country.name.official}</h1>
+    </div>
+    <p class="country-info__details"><b>Capital: </b>${country.capital}</p>
+    <p class="country-info__details"><b>Population: </b>${country.population}</p>
+    <p class="country-info__details"><b>Languages: </b>${Object.values(country.languages)}</p>`
+  ).join(' ');
 };
 
