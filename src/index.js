17125afa4751eac38a71de2331e3b2dfd798b6db
Jose Gonzales <jose.gonzales08@ymail.com>
2023-02-27 19:26:03 -0500

    update
    update

--- src/index.js
+++ src/index.js
@@ -25,15 +25,15 @@ function handleInput(e) {
   countryAPIFetch(inputValue)
     .then((data) => {
       console.log(data);
-      if (data.length > 10) {
+      if (data.length > 20) {
         ref.countryList.innerHTML = '';
         ref.countryInfo.innerHTML = '';
         Notify.info("Too many matches found. Please enter a more specific name.");
         return;
       };
       
-      if (data.length >= 2 && data.length <= 10) {
-        console.log(data.length >= 2 && data.length <= 10);
+      if (data.length >= 2 && data.length <= 20) {
+        console.log(data.length >= 2 && data.length <= 20);
         ref.countryInfo.innerHTML = '';
         const countryListMarkUp = createCountryListMarkup(data);
         ref.countryList.innerHTML = countryListMarkUp;
@@ -56,22 +56,36 @@ function handleInput(e) {
 function createCountryListMarkup(countriesData) {
   return countriesData.map((country) =>
     `<li class="country-list__item">
-      <img class="country-list__img" src="${country.flags.svg}" alt="${country.flags.alt}" width="40" height="20">
-      <p class="country-list__country-name">${country.name.official}</p>
+      <div class="country-list__overlay-container">
+        <img class="country-list__img" src="${country.flags.svg}" alt="${country.flags.alt}" width="200" >
+        <div class="country-list__overlay">
+          <p class="country-list__overlay-text"><b>Capital: </b>${country.capital}</p>
+          <p class="country-list__overlay-text"><b>Population: </b>${country.population}</p>
+          <p class="country-list__overlay-text"><b>Languages: </b>${Object.values(country.languages)}</p>
+        </div>
+      </div>
+      <div class="country-list__text-container">
+        <p class="country-list__country-name">${country.name.official}</p>
+      </div>
     </li>`
   ).join(' ');
 };
 
 function createCountryInfoMarkup(countryData) {
   return countryData.map((country) => 
-    `
-    <div class="country-info__img-header-container">
-    <img class="country-info__img" src="${country.flags.svg}" alt="${country.flags.alt}" width="40" height="20">
-    <h1 class="country-info__header">${country.name.official}</h1>
-    </div>
-    <p class="country-info__details"><b>Capital: </b>${country.capital}</p>
-    <p class="country-info__details"><b>Population: </b>${country.population}</p>
-    <p class="country-info__details"><b>Languages: </b>${Object.values(country.languages)}</p>`
+    `<ul class="country-info__list">
+      <li class="country-info__item">
+        <div class="country-info__img-header-container">
+          <img class="country-info__img" src="${country.flags.svg}" alt="${country.flags.alt}" width="20" height="20">
+          <h1 class="country-info__header">${country.name.official}</h1>
+        </div>
+        <div class="country-info__details-container">
+          <p class="country-info__details"><b>Capital: </b>${country.capital}</p>
+          <p class="country-info__details"><b>Population: </b>${country.population}</p>
+          <p class="country-info__details"><b>Languages: </b>${Object.values(country.languages)}</p>
+        </div>
+      </li>
+    </ul>`
   ).join(' ');
 };
 
